b08ae723206dd7512f4384e391b69cf7aae8563f
Arthur Tavares Quintao <arthurtquintao@gmail.com>
2020-06-22 20:49:51 -0400

    Reorganize all current actions within main()

--- src/app.py
+++ src/app.py
@@ -1,34 +1,22 @@
 from selenium import webdriver
 from time import sleep
-from pages import LoginPage, FeedPage
 import os
-
-
-def login(browser, user_username, user_password):
-    login_page = LoginPage(browser)
-    login_page.login(user_username, user_password)
-
-    errors = browser.find_elements_by_css_selector("#error_message")
-    assert len(errors) == 0
-
-    feed = FeedPage(browser)
-    feed.access_feed(browser)
-    feed.close_popup()
-
-    sleep(5)
-
-    return
+from actions import Instactions
 
 def main():
-    username, password = os.getenv('USERNAME'), os.getenv('PASSWORD')
+    user_username, user_password = os.getenv('USERNAME'), os.getenv('PASSWORD')
 
     # Make sure the env variables were given
     if not os.environ.get('USERNAME') or not os.environ.get('PASSWORD'):
         raise RuntimeError('\nUSERNAME or PASSWORD were not set')
     
     browser = webdriver.Chrome()
-    login(browser, username, password)
-    
+    instactions = Instactions(browser, user_username, user_password)
+
+    instactions.login()
+    instactions.access_feed()
+    instactions.close_popup()
+       
 
 if __name__ == '__main__':
     main()
