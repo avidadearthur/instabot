3f28250a02be2bdd073cc58288e3921a2582f7cd
Arthur Tavares Quintao <arthurtquintao@gmail.com>
2020-08-15 18:14:28 -0400

    Create json file
    .json to keep track of the post list

--- src/app.py
+++ src/app.py
@@ -3,22 +3,19 @@ from time import sleep
 import os
 from actions import Instactions
 from upload import Instaupload
+from posts import PostHandler
 
 def main():
     user_username, user_password = os.getenv('USERNAME'), os.getenv('PASSWORD')
+    path = "/Users/arthurquintao/Desktop/instabot-img/"
 
     # Make sure the env variables were given
     if not os.environ.get('USERNAME') or not os.environ.get('PASSWORD'):
         raise RuntimeError('\nUSERNAME or PASSWORD were not set')
     
-    # browser = webdriver.Chrome()
+    posthandler = PostHandler(path)
     instaupload = Instaupload(user_username, user_password)
-    instaupload.post_photo("Bip Bop tzzz - test01")
-    # instactions = Instactions(browser, user_username, user_password)
-
-    # instactions.login()
-    # instactions.access_feed()
-    # instactions.close_popup()
+    # instaupload.post_photo("Bip Bop tzzz - test01")
        
 
 if __name__ == '__main__':
